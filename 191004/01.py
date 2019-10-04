8c6e3d4d946f7a5e0b8e40d4a2715c66d63b1be1
GyuReeKim <starkim96@naver.com>
2019-10-04 13:35:09 +0900

    algorithm 1004

---
+++ 191004/01.py
@@ -0,0 +1,24 @@
+# 정곤이의 단조 증가하는 수
+
+def increase(a):
+    if len(a) == 1:
+        return -1
+    else:
+        for k in range(len(a)-1):
+            if int(a[k]) > int(a[k+1]):
+                return -1
+        return int(a)
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    maxV = -1
+    for i in range(N):
+        for j in range(i+1, N):
+            a = str(A[i]*A[j])
+            if increase(a) > maxV:
+                maxV = increase(a)
+    print('#{} {}'.format(tc, maxV))
